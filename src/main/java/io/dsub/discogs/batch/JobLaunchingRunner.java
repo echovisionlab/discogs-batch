5a8bdedf696ff076a9b0b84a3c5f128f5b26bf0d
Seunghyun Bosco Bae <shbae0121@gmail.com>
2021-06-16 15:56:48 +0900

    adds test for runners

--- src/main/java/io/dsub/discogs/batch/JobLaunchingRunner.java
+++ src/main/java/io/dsub/discogs/batch/JobLaunchingRunner.java
@@ -3,6 +3,7 @@ package io.dsub.discogs.batch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.ApplicationArguments;
