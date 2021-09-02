0574d7135831e901de32f856c364e68a88618eb8
Seunghyun Bosco Bae <shbae0121@gmail.com>
2021-09-02 15:14:29 +0900

    refit the test cases to be synchronized to actual changes on base code

--- src/test/java/io/dsub/discogs/batch/config/TaskExecutorConfigTest.java
+++ src/test/java/io/dsub/discogs/batch/config/TaskExecutorConfigTest.java
@@ -12,16 +12,18 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import oshi.SystemInfo;
 
 class TaskExecutorConfigTest {
 
   ApplicationContextRunner ctx;
 
-  @RegisterExtension
-  LogSpy logSpy = new LogSpy();
+  @RegisterExtension LogSpy logSpy = new LogSpy();
 
-  int maxCore = Runtime.getRuntime().availableProcessors();
-  int defaultCoreSize = maxCore > 2 ? (int) (maxCore * 0.8) : 1;
+  SystemInfo systemInfo = new SystemInfo();
+
+  int physicalCoreSize = systemInfo.getHardware().getProcessor().getPhysicalProcessorCount();
+  int defaultCoreSize = physicalCoreSize > 2 ? (int) (physicalCoreSize * 0.8) : 1;
 
   @BeforeEach
   void setUp() {
@@ -52,14 +54,14 @@ class TaskExecutorConfigTest {
   @Test
   void givenCoreCountArgExceedsFormulae__ShouldLogDetails() {
     // given
-    ctx = ctx.withBean(DefaultApplicationArguments.class, "--coreCount=" + maxCore);
+    ctx = ctx.withBean(DefaultApplicationArguments.class, "--coreCount=" + physicalCoreSize);
 
     // when
     ctx.run(
         it -> {
           assertThat(it).hasSingleBean(ThreadPoolTaskExecutor.class);
           ThreadPoolTaskExecutor taskExecutor = it.getBean(ThreadPoolTaskExecutor.class);
-          assertThat(taskExecutor.getCorePoolSize()).isEqualTo(defaultCoreSize);
+          assertThat(taskExecutor.getCorePoolSize()).isEqualTo(physicalCoreSize);
         });
 
     // then
@@ -67,8 +69,7 @@ class TaskExecutorConfigTest {
     assertAll(
         () -> assertThat(infoLogs).anyMatch(s -> s.contains("found core count argument")),
         () -> assertThat(infoLogs).anyMatch(s -> s.contains("setting core count")),
-        () -> assertThat(infoLogs).anyMatch(s -> s.contains(String.valueOf(defaultCoreSize))),
-        () -> assertThat(infoLogs).anyMatch(s -> s.contains(String.valueOf(maxCore))));
+        () -> assertThat(infoLogs).anyMatch(s -> s.contains(String.valueOf(physicalCoreSize))));
   }
 
   @Test
